7357aefb70c31ba474655a8f47e89c085dfae5a6
BekirK-C <b.b.kamac@gmail.com>
2022-12-26 14:02:11 +0300

    ForMember ile farklı property isimlerini eşleme

--- FluentValidationApp/Mapping/CustomerProfile.cs
+++ FluentValidationApp/Mapping/CustomerProfile.cs
@@ -8,7 +8,12 @@ namespace FluentValidationApp.Mapping
     {
         public CustomerProfile()
         {
-            CreateMap<Customer, CustomerDto>().ReverseMap();
+            CreateMap<Customer, CustomerDto>()
+                .ForMember(dest => dest.Isim, opt => opt.MapFrom(x => x.Name))
+                .ForMember(dest => dest.Eposta, opt => opt.MapFrom(x => x.Email))
+                .ForMember(dest => dest.Yas, opt => opt.MapFrom(x => x.Age))
+                .ReverseMap();
+
             //CreateMap<CustomerDto, Customer>();   ReverseMap() bu işlemi sağlıyor.
         }
     }
